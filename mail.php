8519ac5554fcb3870aed46a3860a34492cb3ad6c
MUHAMMED YAZEEN A N <27795050+messenger-1012@users.noreply.github.com>
2021-09-02 22:33:19 +0530

    added changes to navbar completed 90%

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message= $_POST['Message'];
 $to = "gthsadimali@gmail.com";
 $subject = "Mail From GTHS Adimali";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@gthsadimali.com" . "\r\n" .
+$headers = "From: admin@gthsadimali.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
