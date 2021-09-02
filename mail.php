80672d3b63fc45de538c10e38aed0377ffead175
MUHAMMED YAZEEN A N <27795050+messenger-1012@users.noreply.github.com>
2021-09-02 18:19:23 +0530

    completed contact us form

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+
+$name = $_POST['Name'];
+$email= $_POST['Email'];
+$message= $_POST['Message'];
+$to = "gthsadimali@gmail.com";
+$subject = "Mail From GTHS Adimali";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@gthsadimali.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
